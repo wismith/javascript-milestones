29587e07c2afafc42e6a07fc16c2bdf85ae5a666
Will Smith <wismith@davidson.edu>
2020-02-10 11:22:37 -0800

    sandbox file

---
+++ tasks-devEnvironment/sandbox.js
@@ -0,0 +1,13 @@
+/* This is a sample file to show competency in using the Development Environment on my computer
+    and on VSCode.
+*/
+
+let myExampleFunction = () => {
+  console.log('Testing, testing, one two three...');
+}
+
+if (require.main === module) {
+  console.log('Testing the function I wrote in this file:');
+  console.log();
+  myExampleFunction();
+}
